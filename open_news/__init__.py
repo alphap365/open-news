6df0eb819653a86107c0a21cd2023c57db0b6a32
A. Paul <arajitpaul2010@gmail.com>
2026-06-02 15:53:31 +0530

    refactor: expose batch processing functions in public API

--- open_news/__init__.py
+++ open_news/__init__.py
@@ -1,10 +1,13 @@
-"""Minimal news fetching package: article text, RSS/Google News URLs, feed discovery."""
+"""Minimal news fetching package: article text, RSS/Google News URLs, feed discovery, batch processing."""
 
 from .main import fetch_article, search_news, get_live_news, get_articles_from_website_rss
+from .batch_processor import fetch_and_summarize_batch, fetch_and_summarize_search_results
 
 __all__ = [
     "fetch_article",
     "search_news",
     "get_live_news",
     "get_articles_from_website_rss",
+    "fetch_and_summarize_batch",
+    "fetch_and_summarize_search_results",
 ]
