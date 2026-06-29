8e09ed0ee8705804b7db384d1b5cb6a24ae307e3
alphap365 <arajitpaul2010@gmail.com>
2026-06-29 13:19:22 +0530

    Changed: Added and fixed some functions

--- open_news/__init__.py
+++ open_news/__init__.py
@@ -1,26 +1,36 @@
-"""Minimal news fetching package: article text, images, videos, RSS, batch summarization."""
+"""Minimal news fetching package: article text, images, videos, RSS,
+Google News, site search, batch summarization, dedupe."""
 
-from .main import (
+from .api import (
     get_article,
     search,
+    search_site,
     live_news,
     discover_and_get,
+    clear_feed_cache,
     # legacy aliases
     fetch_article,
     search_news,
     get_live_news,
     get_articles_from_website_rss,
 )
-from .batch_processor import batch_summarize, search_and_summarize
-from .batch_processor import fetch_and_summarize_batch, fetch_and_summarize_search_results  # legacy
+from .processing.batch import batch_summarize, search_and_summarize
+from .processing.batch import fetch_and_summarize_batch, fetch_and_summarize_search_results  # legacy
+from .processing.dedupe import dedupe_articles
+from .feeds.registry import list_categories, list_countries
 
 __all__ = [
     "get_article",
     "search",
+    "search_site",
     "live_news",
     "discover_and_get",
+    "clear_feed_cache",
     "batch_summarize",
     "search_and_summarize",
+    "dedupe_articles",
+    "list_categories",
+    "list_countries",
     # legacy
     "fetch_article",
     "search_news",
@@ -30,4 +40,4 @@ __all__ = [
     "fetch_and_summarize_search_results",
 ]
 
-__version__ = "0.1.2"
+__version__ = "0.2.0"
