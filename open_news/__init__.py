0d0445d536f661387e427a23eb2150795928fd73
alphap365 <arajitpaul2010@gmail.com>
2026-09-19 01:06:08 +0530

    Testing some new features

--- open_news/__init__.py
+++ open_news/__init__.py
@@ -15,7 +15,7 @@ from .processing.batch import batch_summarize, search_and_summarize
 from .processing.summarizer import summarize_text, summarize_with_keywords
 from .processing.dedupe import dedupe_articles
 
-__version__ = "1.0.3a3"
+__version__ = "1.0.3a4"
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
